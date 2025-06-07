f15b6032b811c0574b91bac8c4dd983198a571f5
snx19 <hayato@msnx.local>
2025-06-06 22:25:03 -0500

    added code for NaiveStrategy

--- src/main/java/NaiveStrategy.java
+++ src/main/java/NaiveStrategy.java
@@ -1,2 +1,15 @@
-public class NaiveStrategy {
+public class NaiveStrategy implements DrawingStrategy {
+    @Override
+    public void drawLine(Matrix m, double x0, double y0, double x1, double y1) {
+        int steps = (int)Math.max(Math.abs(x1 - x0), Math.abs(y1 - y0));
+        double dx = (x1 - x0) / steps;
+        double dy = (y1 - y0) / steps;
+        double x = x0;
+        double y = y0;
+        for (int i = 0; i <= steps; i++) {
+            m.setCell((int)x, (int)y, '#');
+            x += dx;
+            y += dy;
+        }
+    }
 }
